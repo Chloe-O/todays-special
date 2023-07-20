db6449c128aac1f57f9c77c5a80b98e51ec708d8
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-07-20 17:36:26 +0100

    begin styling

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
+const menuItem = document.getElementById('menuItem').value;
+const menuPrice = document.getElementById('menuPrice').value;
+let setTheMenu = document.getElementById('setTheMenu');
+
 const menu = {
     _meal: '',
     _price: 0,
 
-    set meal(mealToCheck) {
-        if (typeof mealToCheck === 'string') {
-            return this._meal = mealToCheck;
+    set meal(menuItem) {
+        if (typeof menuItem === 'string') {
+            return this._meal = menuItem;
         }
     },
-    set price(priceToCheck) {
-        if (typeof priceToCheck === 'number') {
-            return this._price = priceToCheck;
+    set price(menuPrice) {
+        if (typeof menuPrice === 'number') {
+            return this._price = menuPrice;
         }
     },
 
     get todaysSpecial() {
         if (this._meal && this._price) {
             return `Today's special is ${this._meal} for £${this._price.toFixed(2)}`
+
         } else {
             return `Error! Please make sure the meal and price for today's special has been set.`
         }
     }
 };
 
-
-menu.meal = 'Lasagne';
-menu.price = 7.50;
-
-console.log(menu._price);
-console.log(menu._meal);
-
-console.log(menu.todaysSpecial);
+setTheMenu.addEventListener('click', () => {
+console.log(menuItem);
+console.log(menuPrice);
+});
